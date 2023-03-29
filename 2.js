c711ee5f065a391a4a8901bc2759ca9f7b0e470c
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-03-29 15:41:59 +0300

    task 2 solved

--- 2.js
+++ 2.js
@@ -1,1 +1,15 @@
-3
+// Дан массив с числами. Узнайте сколько элементов с начала массива 
+// надо сложить, чтобы в сумме получилось больше 10-ти.
+
+const findNumberOfElem = (arr) => {
+    let sum = 0;
+    let result = 0;
+    for (let i=0; i < arr.length; i++){
+        sum += arr[i];
+        result = i;
+        if (sum > 10) break;
+    }
+    return result+1;
+};
+
+console.log(findNumberOfElem([1, 4, 0.9, 7, 0]));
