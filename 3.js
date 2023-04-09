7f0d57aec9e489615c414067d248f8264180d364
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-04-09 23:44:01 +0300

    solved task 3 with filter() and solved task 4 with reduce()

--- 3.js
+++ 3.js
@@ -4,14 +4,8 @@
 // [0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
 
 const clearNewArr = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        console.log(typeof(arr[i]));
-        console.log(Boolean(arr[i]));
-        if (Boolean(arr[i]) === false) {
-            arr.splice(i, 1);
-        }
-    }
-    return arr;
+    newArr = arr.filter(item => (Boolean(item) == true));
+    return newArr;
 };
 
-console.log(clearNewArr([0, 1, false, 2, undefined, '', 3, null]));
+console.log(clearNewArr([0, 1, false, 2, undefined, '', 3, null, '']));
